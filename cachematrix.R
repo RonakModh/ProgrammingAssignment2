28977e4aaa8b185ea0b13ccc3247b07d2f457ebe
Ronak Modh <ronak.rocks90@gmail.com>
2016-06-25 14:59:42 +0530

    Add existing file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
+# makeCacheMatrix is a function that returns a list of functions
+# Its puspose is to store and return a martix and a cached value of the inverse of the 
+# matrix. Contains the following functions:
+# 1. setMat      set the value of a matrix
+# 2. getMat      get the value of a matrix
+# 3. cacheInv    Cache the value (inverse of the matrix)
+# 4. getInv      get the cahced value (inverse of the matrix)
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric()) {
+  
+  #Cache variable intialized
+  c <- NULL
+  
+  # store a matrix
+  setMat <- function(matrixData) {
+    x <<- matrixData
+    c <<- NULL   # Everytime matrix gets updated, we should clear the cache.  
+  }
+  
+  # returns the stored matrix
+  getMat <- function() {
+    x
+  }
+  
+  # cache the given argument(Inverse of the matrix will get cached) 
+  cacheInv <- function(store) {
+    c <<- store
+  }
+  
+  # get the cached value
+  getInv <- function() {
+    c
+  }
+  
+  list(setMat = setMat, getMat = getMat, cacheInv = cacheInv, getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+# The following function calculates the inverse of a "special" matrix created with 
+# makeCacheMatrix
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(y, ...) {
+  
+  # get the cached value by calling the below fumction
+  inv <- y$getInv()
+  
+  # if a cached value exists return it
+  if(!is.null(inv)) {
+    message("Cached data is as follows")
+    return(inv)
+  }
+  
+  # otherwise get the matrix, caclulate the inverse and store it in
+  # the cache
+  data <- y$getMat()
+  inv <- solve(data) #solve() --> This function calculates the inverse of a square matrix
+  y$cacheInv(inv)
+  
+  # return the inverse
+  inv
 }
